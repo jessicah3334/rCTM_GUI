b5e545a6a3a9fec9e3f2e97728ee4bdf4d726b0d
Jessica Hicks <jhicks1@ufl.edu>
2020-06-29 14:15:01 -0400

    Switched over to Dashboard layout

--- rCTM_v6.0/server.R
+++ rCTM_v6.0/server.R
@@ -6,9 +6,6 @@
 #
 #    http://shiny.rstudio.com/
 #
-library(shiny)
-#library(rCTM)
-  # currently using local functions for debugging purposes
 
 # Define server logic
 shinyServer(function(input, output) {
@@ -16,37 +13,23 @@ shinyServer(function(input, output) {
 #Make a reactive function to populate inputParms
   getParms <- reactive({
     
-    inputParms <- list(startYear = input$startYear, endYear = input$endYear, relSeaLevelRiseInit = input$relSeaLevelRiseInit, relSeaLevelRiseTotal = 100,
+    inputParms <- list(startYear = input$startYear, endYear = input$endYear, relSeaLevelRiseInit = input$relSeaLevelRiseInit, rslrTotal = 100,
                        initElv = input$initElev, meanSeaLevel = input$meanSeaLevel, meanSeaLevelDatum = input$meanSeaLevelDatum, meanHighWater = input$meanHighWater,
                        meanHighHighWater = input$meanHighHighWater, meanHighHighWaterSpring = input$meanHighHighWaterSpring, suspendedSediment = input$suspendedSediment,
                        lunarNodalAmp = input$lunarNodalAmp, bMax = input$bMax, zVegMin = input$zVegMin, zVegMax = input$zVegMax, zVegPeak = input$zVegPeak,
                        plantElevationType = input$planeElevationType, rootToShoot = input$rootToShoot, rootTurnover = input$rootTurnover, rootDepthMax = input$rootDepthMax,
                        shape = input$shape, omDecayRate = input$omDecayRate, recalcitrantFrac= input$recalcitrantFrac, settlingVelocity = input$settlingVelocity,
                        omPackingDensity = input$omPackingDensity, mineralPackingDensity = input$mineralPackingDensity, rootPackingDensity = input$rootPackingDensity,
-                       coreYear = input$coreYear, coreDepth = input$coreDepth, coreMaxs = 1:input$coreDepth, coreMins = 1:input$coreDepth-1)
+                       coreYear = input$coreYear, coreDepth = input$coreDepth, coreMaxs = input$coreMaxs, coreMins = input$coreMins)
     print(inputParms)
     return(inputParms)
   })
   
   
-# Run the function runMemWithCohorts with GUI inputs
+#run the function runMemWithCohorts with GUI inputs
   runSim <- eventReactive(input$run_sim, {
-    do.call(runMemWithCohorts, getParms())
-  })
-
-# Run "makePlots" and add plots to GUI window
-  graphs <- eventReactive(input$run_sim, {
-    modelOutput <- do.call(runMemWithCohorts, getParms())
-    print(modelOutput)
-    makePlots(modelOutput)
-  })  
-  
-  output$plot1 <- renderPlot({
-    graphs()$plot1
+    do.call(rCTM::runMemWithCohorts, getParms())
   })
   
-  output$plot2 <- renderPlot({
-    graphs()$plot2
-  })
   
 })
--- rCTM_v6.0/ui.R
+++ rCTM_v6.0/ui.R
@@ -7,78 +7,84 @@
 #    http://shiny.rstudio.com/
 #
 library(shiny)
+library(shinydashboard)
+library(rCTM)
 
-shinyUI(fluidPage(
-    
+shinyUI(fluidPage(dashboardPage(
+    skin = "green",
+    title = "Marsh Equilibrium Cohort Model v",
+
 # HEADER ---------------------------------------------------------------------
-titlePanel("Marsh Equilibrium Model with Cohorts"),
+    dashboardHeader(
+        title = "Marsh Equilibrium Cohort Model v",
+        titleWidth = 500
+    ),
 
-sidebarLayout(
 # SIDEBAR --------------------------------------------------------------------
-    sidebarPanel(
+    dashboardSidebar(
+        width = 350,
         fluidRow(
-        column(12,
-            actionButton("run_sim", "Run Simulation"),
-            actionButton("restore_inputs", "Restore Inputs"), align = "center"),
-        hr(),
-        h4("Options", align = "center"),
-            
-        h4("Physical Inputs", align = "center"),
-        column(12,   
+            column(4,
+                   actionButton("run_sim", "Run Simulation")),
             column(6,
-                sliderInput("startYear", "Start Year", 1500, 2500, 2020),
-                sliderInput("endYear", "End Year", 1500, 2500, 2119),
-                sliderInput("rslrTotal", "Century Sea Level Rise", 0, 100, 100),
-                sliderInput("relSeaLevelRiseInit", "Initial Sea Level Rise", 0, 10, 0.3, step = 0.1),
-                sliderInput("meanHighWater", "Mean High Water", 0, 20, 16.9, step = 0.1),
-                sliderInput("meanHighHighWater", "Mean High High Water", 0, 999, 25.4, step = 0.1),
-                sliderInput("meanHighHighWaterSpring", "Mean High High Water Spring", 0, 999, 31.2, step = 0.1),
-                sliderInput("meanSeaLevelDatum", "Mean Sea Level", 0, 999, 7.4, step = 0.1),
-                sliderInput("settlingVelocity", "Settling Velocity", 0, 999, 2.8, step = 0.1)),
-            column(6,
-                sliderInput("lunarNodalAmp", "Lunar Nodal Amp", 0, 999, 2.5, step = 0.1),
-                sliderInput("meanSeaLevel", "Initial Rate SLR", 0, 999, 7.4, step = 0.1),
-                sliderInput("suspendedSediment", "Susp. Sediment Conc.", 0, 1, 3e-05, step = 0.00001),
-                sliderInput("initElev", "Marsh Elevation", 0, 999, 21.9, step = 0.1),
-                sliderInput("coreYear", "Core Year", 0, 9999, 2050),
-                sliderInput("coreDepth", "Core Depth", 0, 999, 100),
-                sliderInput("coreMaxs", "Core Max", 0, 999, 999),
-                sliderInput("coreMins", "Core Mins", 0, 999, 999))),
+                   actionButton("restore_inputs", "Restore Inputs"))),
+        menuItem(
+            "OPTIONS",
+            tabName = "Options",
+            icon = icon("spinner")
+        ),
+        menuItem(
+            "PHYSICAL INPUTS",
+            tabName = "Physical Inputs",
+            icon = icon("spinner"),
+            sliderInput("startYear", "Start Year", 1500, 2500, 2020),
+            sliderInput("endYear", "End Year", 1500, 2500, 2119),
+            sliderInput("rslrTotal", "Century Sea Level Rise", 0, 100, 100),
+            sliderInput("relSeaLevelRiseInit", "Initial Sea Level Rise", 0, 10, 0.3),
+            sliderInput("meanHighWater", "Mean High Water", 0, 999, 16.9),
+            sliderInput("meanHighHighWater", "Mean High High Water", 0, 999, 25.4),
+            sliderInput("meanHighHighWaterSpring", "Mean High High Water Spring", 0, 999, 31.2),
+            sliderInput("meanSeaLevelDatum", "Mean Sea Level", 0, 999, 7.4),
+            sliderInput("settlingVelocity", "Settling Velocity", 0, 999, 2.8),
+            sliderInput("lunarNodalAmp", "Lunar Nodal Amp", 0, 999, 2.5),
+            sliderInput("meanSeaLevel", "Initial Rate SLR", 0, 999, 7.4),
+            sliderInput("suspendedSediment", "Susp. Sediment Conc.", 0, 1, 3e-05),
+            sliderInput("initElev", "Marsh Elevation", 0, 999, 21.9),
+            sliderInput("coreYear", "Core Year", 0, 9999, 2050),
+            sliderInput("coreDepth", "Core Depth", 0, 999, 100),
+            sliderInput("coreMaxs", "Core Max", 0, 999, 999),
+            sliderInput("coreMins", "Core Mins", 0, 999, 999)
+        ),
+        menuItem(
+            "BIOLOICAL INPUTS",
+            tabName = "Biological Inputs",
+            icon = icon("spinner"),
+            sliderInput("zVegMax", "Max Veg. Elev.", 0, 999, 44.4),
+            sliderInput("zVegMin", "Min Veg. Elev.", -30, 999, -24.7),
+            sliderInput("zVegPeak", "Peak Veg. Elev.", 0, 999, 22.1),
+            sliderInput("bMax", "Max Biomass", 0, 999, 0.25),
+            sliderInput("omDecayRate", "OM Decay Rate", 0, 999, 0.8),
+            sliderInput("rootToShoot", "Root to Shoot Ratio", 0, 999, 2),
+            sliderInput("rootTurnover", "BG Turnover Rate", 0, 999, 0.5),
+            sliderInput("rootDepthMax", "Max (95%) Root Depth", 0, 999, 30),
+            sliderInput("omPackingDensity", "Organic Packing Density", 0, 1, 0.085),
+            sliderInput("mineralPackingDensity", "Mineral Packing Density", 0, 2, 1.99),
+            sliderInput("rootPackingDensity", "Root Packing Density", 0, 1, 0.085),
+            selectInput("planeElevationType", "Plant Elevation Type", c("orthometric", "dimensionless")),
+            selectInput("shape", "Root Shape", c("linear", "exponential"))
+        ),
+        menuItem(
+            "MODEL DERIVED INPUTS",
+            tabName = "Model Derived Inputs",
+            icon = icon("spinner"),
+            sliderInput("recalcitrantFrac", "Recalcitrant Fraction", 0, 1, 0.2)
+        )
+    
+        
+    ),
 
-        h4("Biological Inputs", align = "center"),
-        column(12,   
-            column(6,
-                sliderInput("zVegMax", "Max Veg. Elev.", 0, 999, 44.4),
-                sliderInput("zVegMin", "Min Veg. Elev.", -30, 999, -24.7),
-                sliderInput("zVegPeak", "Peak Veg. Elev.", 0, 999, 22.1),
-                sliderInput("bMax", "Max Biomass", 0, 999, 0.25, step = 0.1),
-                sliderInput("omDecayRate", "OM Decay Rate", 0, 999, 0.8),
-                sliderInput("rootToShoot", "Root to Shoot Ratio", 0, 999, 2),
-                sliderInput("rootTurnover", "BG Turnover Rate", 0, 999, 0.5, step = 0.1)),
-            column(6,
-                sliderInput("rootDepthMax", "Max (95%) Root Depth", 0, 999, 30),
-                sliderInput("omPackingDensity", "Organic Packing Density", 0, 1, 0.085),
-                sliderInput("mineralPackingDensity", "Mineral Packing Density", 0, 2, 1.99),
-                sliderInput("rootPackingDensity", "Root Packing Density", 0, 1, 0.085),
-                selectInput("planeElevationType", "Plant Elevation Type", c("orthometric", "dimensionless")),
-                selectInput("shape", "Root Shape", c("linear", "exponential")))),
-            
-        h4("Model Derived Inputs", align = "center"),
-        column(12,
-            column(6,
-                sliderInput("recalcitrantFrac", "Recalcitrant Fraction", 0, 1, 0.2))
-            
-))),
-
-# MAIN PANEL ----------------------------------------------------------------------
-    mainPanel(
-        tabsetPanel(
-            tabPanel("Plots", plotOutput("plot1"), plotOutput("plot2")),
-            tabPanel("Animation"),
-            tabPanel("Model Diagram"),
-            tabPanel("Parameter Ranges"),
-            tabPanel("R Code for Parameterization")
-        ) 
+# MAIN PANEL -----------------------------------------------------------------
+    dashboardBody(
+        
     )
-
 )))
